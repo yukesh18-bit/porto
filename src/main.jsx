8ff2d4bb789798d5330c9e9bcfbb99726192a3da
Yukesh <yukeshkumarviiib123@gmail.com>
2026-08-02 20:06:06 +0530

    Fix UserContext filename

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { UserProvider } from "./context/UserContext";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserProvider>
