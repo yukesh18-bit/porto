be68f24d2169464667e4ed6c3d9c59b2860db6de
Yukesh <yukeshkumarviiib123@gmail.com>
2026-08-04 18:17:45 +0530

    Update backend URL

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -23,7 +23,7 @@ const [password, setPassword] = useState("");
 const handleLogin = async () => {
   try {
     const res = await axios.post(
-      "http://localhost:5000/api/auth/login",
+      "https://portoo-backend.onrender.com",
       {
         email,
         password,
